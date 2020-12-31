da62e04c0a1f06a0e4f209a734b8a5efba62d5fd
Jatin Kumar <45680394+jk12873@users.noreply.github.com>
2020-12-31 13:54:25 +0530

    Add files via upload
    Some more pages

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div class="wrapper col5">
+  <div id="footer">
+
+    <div id="copyright">
+      <p class="fl_left">Copyright &copy; 2020| <a href="adminlogin.php">Admin Login Panel</a> | <a href="doctorlogin.php">Doctor Login Panel</a></p>
+      <p class="fl_right">Developed by <a href="https://www.jk12873.com">18BCB0072 and 18BCB0025</a></p>
+      <br class="clear" />
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+</body>
+</html>
